ec9ad5c04c3cccbea2ef9ed0af0f00ed4171b731
Vladimir <masterfm65@gmail.com>
2024-03-14 19:55:58 +0300

    Starting our work

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def main():
-    pass
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "text"
 
 if __name__ == "__main__":
-    main()
+    app.run()
