9d95d2d33b8110876e8f052366622c64b9fa09f9
rezect <ilyapr1568@yandex.ru>
2024-03-29 13:19:15 +0300

    Добавлено:  - Получение картинок  - Получение отзывов  - Получение наград фильмов  - Обновлена доукментация

--- API/api_realization.py
+++ API/api_realization.py
@@ -1,2 +1,10 @@
 import requests
+from urllib.parse import quote
 
+
+headers = {
+    "accept": "application/json",
+    "X-API-KEY": "S2F7CKC-CTEMPQ1-K4YDW4W-NJRG7KH"
+}
+
+# Пример получения фильма по следующим параметрам: комедия, 2018, рейтинг >= 6
