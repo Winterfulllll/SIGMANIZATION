f74f446c93a3251f64a3a175bf8e19d061991ff9
Vladimir <masterfm65@gmail.com>
2024-05-02 23:59:52 +0300

    registration-preferences-integration-and-code-redesign

--- static/mainpage.js
+++ static/mainpage.js
@@ -8,6 +8,7 @@ function registerUser() {
     patronymic: document.getElementById("patronymic").value,
   };
 
+  // Регистрация пользователя
   fetch("/api/users", {
     method: "POST",
     headers: {
@@ -19,7 +20,64 @@ function registerUser() {
     .then((response) => response.json())
     .then((data) => {
       if (data && data.username) {
-        alert("Пользователь успешно зарегистрирован!");
+        // Отправка предпочтений после успешной регистрации
+        const username = data.username;
+        // Получаем выбранные предпочтения
+
+        const selectedPrefGenres = [];
+        const selectedPrefCountries = [];
+        const selectedPrefYears = [];
+
+        preferenceCheckboxes.forEach((checkbox) => {
+          if (checkbox.checked) {
+            switch (checkbox.name) {
+              case "preference-genre":
+                selectedPrefGenres.push(checkbox.dataset.value);
+                break;
+              case "preference-country":
+                selectedPrefCountries.push(checkbox.dataset.value);
+                break;
+              case "preference-year":
+                selectedPrefYears.push(checkbox.dataset.value);
+                break;
+            }
+          }
+        });
+
+        // Создаём массив промисов для отправки предпочтений
+        const preferencePromises = [];
+        selectedPrefGenres.forEach((genre) =>
+          preferencePromises.push(
+            sendPreference(username, "GENRE", "MOVIE", genre)
+          )
+        );
+        selectedPrefCountries.forEach((country) =>
+          preferencePromises.push(
+            sendPreference(username, "COUNTRY", "MOVIE", country)
+          )
+        );
+        selectedPrefYears.forEach((year) =>
+          preferencePromises.push(
+            sendPreference(username, "YEAR", "MOVIE", year)
+          )
+        );
+
+        // Отправляем все предпочтения и обрабатываем результат
+        Promise.all(preferencePromises)
+          .then(() => {
+            alert("Пользователь и предпочтения успешно сохранены!");
+          })
+          .catch((error) => {
+            fetch(`/api/users/${username}`, {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+                "API-KEY": service_api_key,
+              },
+            });
+            console.error("Ошибка при сохранении предпочтений:", error);
+            alert("Произошла ошибка при сохранении предпочтений.");
+          });
       } else {
         alert("Произошла ошибка при регистрации.");
       }
@@ -27,6 +85,23 @@ function registerUser() {
     .catch((error) => {
       console.error("Ошибка при регистрации:", error);
     });
+}
+
+function sendPreference(username, type, category, typeValue) {
+  const preferenceData = {
+    type: type,
+    category: category,
+    type_value: typeValue,
+  };
+
+  return fetch(`/api/preferences/${username}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "API-KEY": service_api_key,
+    },
+    body: JSON.stringify(preferenceData),
+  });
 }
 
 function loginUser() {
@@ -126,6 +201,16 @@ loginBtn.addEventListener("click", function () {
 
 // =============================================================================================================================
 
+const allCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+
+const filterCheckboxes = document.querySelectorAll(
+  'input[name="filters-genre"], input[name="filters-country"], input[name="filters-year"], input[name="filters-rating"]'
+);
+
+const preferenceCheckboxes = document.querySelectorAll(
+  'input[name="preference-genre"], input[name="preference-country"], input[name="preference-year"]'
+);
+
 let currentPage = 1;
 
 let onPage = 5;
@@ -176,6 +261,7 @@ function fetchMovies() {
       console.error("Ошибка при получении данных:", error);
     });
 }
+document.addEventListener('DOMContentLoaded', fetchMovies);
 */
 function fetchMoviesByFilters(
   genres = [],
@@ -230,7 +316,7 @@ function fetchMoviesByFilters(
 
       // Обновляем состояние кнопок
       prevButton.style.display = currentPage === 1 ? "none" : "block";
-      nextButton.disabled = currentPage * onPage >= totalMovies;
+      nextButton.style.display = currentPage * onPage >= totalMovies ? "none" : "block";
 
       prevButton.removeEventListener("click", handlePrevClick);
       nextButton.removeEventListener("click", handleNextClick);
@@ -243,10 +329,9 @@ function fetchMoviesByFilters(
     });
 }
 
-// Обработчик для всех чекбоксов
-const allCheckboxes = document.querySelectorAll('input[type="checkbox"]');
-allCheckboxes.forEach((checkbox) => {
+filterCheckboxes.forEach((checkbox) => {
   checkbox.addEventListener("change", () => {
+    currentPage = 1;
     fetchMoviesAndUpdate();
   });
 });
@@ -270,19 +355,19 @@ function fetchMoviesAndUpdate() {
   const selectedCountries = [];
   const selectedYears = [];
   const selectedRatings = [];
-  allCheckboxes.forEach((checkbox) => {
+  filterCheckboxes.forEach((checkbox) => {
     if (checkbox.checked) {
       switch (checkbox.name) {
-        case "genre":
+        case "filters-genre":
           selectedGenres.push(checkbox.dataset.value);
           break;
-        case "country":
+        case "filters-country":
           selectedCountries.push(checkbox.dataset.value);
           break;
-        case "year":
+        case "filters-year":
           selectedYears.push(checkbox.dataset.value);
           break;
-        case "rating":
+        case "filters-rating":
           selectedRatings.push(checkbox.dataset.value);
           break;
       }
@@ -324,5 +409,3 @@ function createMovieElement(movie) {
   // Возвращаем готовый элемент
   return movieElement;
 }
-
-// document.addEventListener('DOMContentLoaded', fetchMovies);
--- static/profile.js
+++ static/profile.js
@@ -5,7 +5,40 @@ function fetchViewedFilms() {
       "Content-Type": "application/json",
       "API-KEY": service_api_key,
     },
-  });
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const movieIds = data.map((review) => review.item_id); // Получаем список ID фильмов
+      const moviesContainer = document.getElementById("movies-container");
+      moviesContainer.innerHTML = ""; // Очищаем контейнер
+
+      // Для каждого ID выполняем запрос в Kinopoisk API
+      movieIds.forEach((movieId) => {
+        fetch(
+          `https://api.kinopoisk.dev/v1.4/movie/${movieId}?selectFields=id&selectFields=name&selectFields=poster¬NullFields=id¬NullFields=name¬NullFields=poster.url`,
+          {
+            method: "GET",
+            headers: {
+              "X-API-KEY": movies_api_key,
+              "Content-Type": "application/json",
+            },
+          }
+        )
+          .then((response) => response.json())
+          .then((movieData) => {
+            const movieElement = createMovieElement(movieData);
+            if (movieElement) {
+              moviesContainer.appendChild(movieElement);
+            }
+          })
+          .catch((error) => {
+            console.error("Ошибка при получении данных фильма:", error);
+          });
+      });
+    })
+    .catch((error) => {
+      console.error("Ошибка при получении просмотренных фильмов:", error);
+    });
 }
 
 function createMovieElement(movie) {
